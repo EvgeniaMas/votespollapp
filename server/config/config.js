102d83e95ef1837800f0e8a84d454201b9c45995
Евгения Масликова < >
2017-09-20 11:30:52 +0300

    mongo

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,6 @@
 module.exports = {
     // Uncomment to connect with MongoDB on Cloud
     //'url' : 'mongodb://mvc-user-connect:opklnm@ds019028.mlab.com:19028/mvc-app'
-    'url' : 'mongodb://localhost/mvc-app-multimedia'
+    //'url' : 'mongodb://localhost/mvc-app-multimedia'
+     'url' : 'mongodb://bradly:bradly@ds143734.mlab.com:43734/votespoll'
 };
